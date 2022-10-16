16982febc0a03ae4c520e938fdcde7dedd9bc3b2
Carlos S <camilo.suarez85@gmail.com>
2022-10-15 20:45:26 -0500

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Camilo on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
